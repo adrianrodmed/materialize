297486b6df4f0a4369726050e92eae49bfb88aa4
adrianrodmed <34574699+adrianrodmed@users.noreply.github.com>
2017-12-22 13:51:24 +0100

    Add files via upload

---
+++ 10_materialize/WebContent/public/js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+	  $('.button-collapse').sideNav();
+	  $('.parallax').parallax();
+	  $('.carousel').carousel();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
